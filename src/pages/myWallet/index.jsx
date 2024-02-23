13ebd9f532e979474b167dbd822fb487aab92e93
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-23 19:08:11 +0530

    my-wallet-v2 page completed

--- src/pages/myWallet/index.jsx
+++ src/pages/myWallet/index.jsx
@@ -6,19 +6,25 @@ import EfficiencyV2 from "../../component/revenueFlow/EfficiencyV2";
 
 function MyWallet() {
   return (
-    <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
-      <div className="2xl:flex 2xl:space-x-[48px]">
-        <section className="2xl:w-[424px]">
-          <AddBalance />
-          <Wallet />
-        </section>
-        <div className="2xl:flex-1">
-          <section className="w-full xl:flex xl:space-x-[24px]">
-            <SummaryV3 />
-            <EfficiencyV2 />
-          </section>
-          <ListTab />
-        </div>
+    <main className="w-full 2xl:grid 2xl:grid-cols-6 gap-4 xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
+      <div className="xl:col-span-4">
+        <ListTab />
+      </div>
+
+      <div className="mt-6 2xl:mt-0 xl:col-span-2">
+        <Wallet />
+      </div>
+
+      <div className="mt-6 2xl:mt-0  col-span-2">
+        <AddBalance />
+      </div>
+
+      <div className="col-span-2">
+        <SummaryV3 />
+      </div>
+
+      <div className="col-span-2">
+        <EfficiencyV2 />
       </div>
     </main>
   );
