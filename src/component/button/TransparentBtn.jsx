d0589fc5dae88b34076989e760365941799d64ee
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-23 17:45:19 +0530

    my-wallet-first-commit

---
+++ src/component/button/TransparentBtn.jsx
@@ -0,0 +1,13 @@
+function TransparentBtn({ title, action }) {
+  return (
+    <button
+      aria-label="none"
+      onClick={() => (action ? action() : "")}
+      className="py-3 px-6 border bg-white border-bgray-500 rounded-lg text-sm font-medium text-bgray-600 dark:bg-darkblack-600 dark:text-white"
+    >
+      {title}
+    </button>
+  );
+}
+
+export default TransparentBtn;
