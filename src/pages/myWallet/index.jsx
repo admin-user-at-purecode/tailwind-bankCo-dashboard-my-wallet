6878fac8764a9251a4fd23191b54d1d5defc620d
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-24 19:38:52 +0530

    cr_3 my wallet some components resized

--- src/pages/myWallet/index.jsx
+++ src/pages/myWallet/index.jsx
@@ -6,19 +6,23 @@ import EfficiencyV2 from "../../component/revenueFlow/EfficiencyV2";
 
 function MyWallet() {
   return (
-    <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
-      <div className="2xl:flex 2xl:space-x-[48px]">
-        <section className="2xl:w-[424px]">
-          <AddBalance />
-          <Wallet />
-        </section>
-        <div className="2xl:flex-1">
-          <section className="w-full xl:flex xl:space-x-[24px]">
-            <SummaryV3 />
-            <EfficiencyV2 />
-          </section>
-          <ListTab />
-        </div>
+    <main className="w-full 2xl:grid gap-6 grid-cols-7 xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
+      <div className="col-span-7">
+        <SummaryV3 />
+      </div>
+      <div className="col-span-4">
+        <AddBalance />
+      </div>
+      <div className="col-span-3">
+        <Wallet />
+      </div>
+
+      <div className="col-span-4 lg:mt-8 2xl:-mt-[13rem]">
+        <ListTab />
+      </div>
+
+      <div className="col-span-3">
+        <EfficiencyV2 />
       </div>
     </main>
   );
