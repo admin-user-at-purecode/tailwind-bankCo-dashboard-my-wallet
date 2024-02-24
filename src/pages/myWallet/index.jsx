fcfb3ae34a70a9825d8fcc9173e6688f5cb4b17c
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-24 15:01:04 +0530

    cd_4 my wallet add balance component deleted

--- src/pages/myWallet/index.jsx
+++ src/pages/myWallet/index.jsx
@@ -9,7 +9,6 @@ function MyWallet() {
     <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:w-[424px]">
-          <AddBalance />
           <Wallet />
         </section>
         <div className="2xl:flex-1">
