2e2d79b4b1037bd4db1e835c3a9dbb271946f6ad
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-25 15:25:31 +0530

    deleted unnecessary files

--- src/pages/myWallet/index.jsx
+++ src/pages/myWallet/index.jsx
@@ -2,7 +2,6 @@ import ListTab from "../../component/listTab";
 import Wallet from "../../component/wallet";
 import AddBalance from "../../component/addBalance/AddBalance";
 import SummaryV3 from "../../component/summary/SummaryV3";
-import EfficiencyV2 from "../../component/revenueFlow/EfficiencyV2";
 
 function MyWallet() {
   return (
@@ -15,9 +14,8 @@ function MyWallet() {
         <div className="2xl:flex-1">
           <section className="w-full xl:flex xl:space-x-[24px]">
             <SummaryV3 />
-            <EfficiencyV2 />
           </section>
-         
+          <ListTab />
         </div>
       </div>
     </main>
