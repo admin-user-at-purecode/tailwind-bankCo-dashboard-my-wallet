e2297e052c0a114ae5b4ebbc44f1253dd080b886
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-25 15:52:22 +0530

    extra components added

--- src/pages/myWallet/index.jsx
+++ src/pages/myWallet/index.jsx
@@ -6,20 +6,38 @@ import EfficiencyV2 from "../../component/revenueFlow/EfficiencyV2";
 
 function MyWallet() {
   return (
-    <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
-      <div className="2xl:flex 2xl:space-x-[48px]">
-        <section className="2xl:w-[424px]">
+    <main className="w-full 2xl:grid gap-8 grid-cols-7 xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
+     
+        <div className="col-span-4">
           <AddBalance />
+        </div>
+        <div className="col-span-3">
           <Wallet />
-        </section>
-        <div className="2xl:flex-1">
-          <section className="w-full xl:flex xl:space-x-[24px]">
-            <SummaryV3 />
-            <EfficiencyV2 />
-          </section>
+        </div>
+        <div className="col-span-3">
+          <SummaryV3 />
+        </div>
+        <div className="col-span-4">
+          <EfficiencyV2 />
+        </div>
+        <div className="col-span-4">
           <ListTab />
         </div>
-      </div>
+
+        <div className="col-span-3">
+          <AddBalance />
+        </div>
+        <div className="col-span-4">
+          <Wallet />
+        </div>
+        <div className="col-span-3">
+          <SummaryV3 />
+        </div>
+        <div className="col-span-3">
+          <EfficiencyV2 />
+        </div>
+     
+     
     </main>
   );
 }
